be5472d5c9969d34acbed17b0a1555f17769b974
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-01-25 22:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+let displayText=document.getElementById('evaluatedText')
+let displayCount=document.getElementById('letterCount')
+ 
+displayText.onkeyup= () =>{
+	let count=0;
+	for(letter of displayText.value)
+		{
+			count++
+		}
+	displayCount.innerText = count
+}
+ 
